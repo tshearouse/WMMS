be0289e7e89d7a4269d5721362d81f3eacbe459a
Tim Shearouse <t.shearouse@gmail.com>
2015-08-23 20:18:33 -0500

    Remove all other roles when the 'none' role is added

--- business_layer/roles.php
+++ business_layer/roles.php
@@ -33,6 +33,11 @@ abstract class UserRoles {
 		return array_keys($constants);
 	}
 	
+	public static function listAllRoleIds() {
+		$constants = self::getConstants();
+		return array_values($constants);
+	}
+	
 	private static function getConstants() {
 		if(self::$enumValues != null) {
 			return $enumValues;
@@ -64,6 +69,11 @@ function AddRoleToUser($userId, $roleName) {
 	AdminOrBoardRightsOrDie();
 
 	require_once('../db/role.php');
+	if($roleName == UserRoles::None) {
+		foreach (UserRoles::listAllRoleIds() as $roleId) {
+			db_RemoveRoleFromUser($userId, $roleId);
+		}
+	}
 	db_AddRoleToUser($userId, $roleName);
 }
 
